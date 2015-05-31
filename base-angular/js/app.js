9be35ec7311dfeabc3c93e65c378cf9bf51ee9cf
phoenix <fy24h.huang@gmail.com>
2015-05-31 22:24:16 +0800

    angular basic

--- base-angular/js/app.js
+++ base-angular/js/app.js
@@ -4,4 +4,20 @@ angular.module('firstApp',[])
 
         vm.message = 'welcome to use angular.';
 
+        vm.computers = [
+            { name:"mac book", color: 'silver' },
+            { name:"yoga 2 pro", color: 'gray' }
+        ];
+
+        vm.computerData={};
+        vm.addComputer = function(){
+            vm.computers.push({
+                name:vm.computerData.name,
+                color:vm.computerData.color
+            });
+
+            vm.computerData = {};
+        };
+
+
     });
